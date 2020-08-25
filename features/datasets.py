351f556ec1ffa00e416142dba0b25f7dcd0303a7
Austin Clyde <aclyde@uchicago.edu>
2020-08-25 13:35:59 -0500

    updated to output inference for more analysi

--- features/datasets.py
+++ features/datasets.py
@@ -202,6 +202,9 @@ class ImageDatasetInfer(Dataset):
                 mol     = Chem.MolFromSmiles(self.smiles[item])
                 image =     transforms.ToTensor()(Invert()(smiles_to_image(mol)))
             except:
+                print(self.smiles)
+                print(self.smiles[item])
+                print(image)
                 image = torch.from_file(np.zeros((3,128, 128, 3)))
             # property = self.property_func(mol)
             #
